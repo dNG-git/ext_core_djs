0b92526bf87981bedae4f542067955dbb0963001
NotTheEvilOne <git-NTEO@vplace.de>
2015-12-04 16:13:04 +0100

    Enhancement: Added support to "file" input elements Change: Generalized module specific "data-*" attribute and CSS naming Trivial: Used short-hand Hammer listener definition

--- src/XHtml5FormElement.js
+++ src/XHtml5FormElement.js
@@ -17,36 +17,23 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 #echo(__FILEPATH__)#
 */
 
-define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
+define([ 'jquery', 'Hammer', 'Modernizr' ], function($, Hammer, Modernizr) {
+	var NEW_INPUT_ATTRIBUTES_COPIED = [ 'id', 'name', 'type', 'size', 'class', 'style', 'multiple', 'tabindex' ];
 	var tabindex_count = 1;
-
-	function enable_input(id) {
-		$("#" + id).removeAttr('disabled');
-	}
-
-	function unfocus(id, focused_class) {
-		$("#" + id).removeClass(focused_class);
-	}
 
 	function XHtml5FormElement(args) {
 		if (args === undefined || (!('id' in args)) || (!('type' in args))) {
 			throw new Error('Missing required arguments');
 		}
 
-		if (!('_djt_XHtml5FormElement_enable_input' in self)) {
-			self._djt_XHtml5FormElement_enable_input = enable_input;
-		}
-
-		if (!('_djt_XHtml5FormElement_unfocus' in self)) {
-			self._djt_XHtml5FormElement_unfocus = unfocus;
-		}
-
 		this.id = args.id;
 		this.$node = $("#" + args.id);
 		this.type = args.type;
 
-		this.focused_class = (('djt_config' in self && 'XHtml5FormElement_focused_class' in self.djt_config) ? self.djt_config.XHtml5FormElement_focused_class : 'djt-ui-XHtml5FormElement-input-focus');
+		this.focused_class = (('djt_config' in self && 'XHtml5FormElement_focused_class' in self.djt_config) ? self.djt_config.XHtml5FormElement_focused_class : 'djt-ui-xhtml5formelement-input-focus');
 		this.focused_duration = (('djt_config' in self && 'XHtml5FormElement_focused_duration' in self.djt_config) ? self.djt_config.XHtml5FormElement_focused_duration : 0);
+		this.selected_file_class = (('djt_config' in self && 'XHtml5FormElement_selected_file_class' in self.djt_config) ? self.djt_config.XHtml5FormElement_selected_file_class : 'djt-ui-xhtml5formelement-input-selected-file');
+		this.selected_file_details_class = (('djt_config' in self && 'XHtml5FormElement_selected_file_details_class' in self.djt_config) ? self.djt_config.XHtml5FormElement_selected_file_details_class : 'djt-ui-xhtml5formelement-input-selected-file-details');
 		this.temporarily_disabled_duration = (('djt_config' in self && 'XHtml5FormElement_temporarily_disabled_duration' in self.djt_config) ? self.djt_config.XHtml5FormElement_temporarily_disabled_duration : 3000);
 
 		var _this = this;
@@ -56,7 +43,7 @@ define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
 				_this.focus();
 			});
 
-			this.tabindex();
+			this.propagate_tabindex();
 		}
 
 		if (this.type == 'button') {
@@ -65,6 +52,10 @@ define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
 					_this.disable_input_temporarily(event);
 				})
 			}
+		}
+
+		if (this.type == 'file') {
+			this._init_file_input();
 		}
 
 		if (this.$node.attr('placeholder') != undefined
@@ -93,7 +84,10 @@ define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
 			_return = false;
 		} else {
 			this.$node.attr('disabled', 'disabled');
-			self.setTimeout('self._djt_XHtml5FormElement_enable_input("' + id + '")', duration);
+
+			self.setTimeout(function() {
+				$("#" + id).removeAttr('disabled');
+			}, duration);
 		}
 
 		if (event == null) {
@@ -109,15 +103,122 @@ define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
 		}
 
 		if (this.$node.addClass(this.focused_class) != null) {
-			if (duration > 0) {
-				self.setTimeout('self._djt_XHtml5FormElement_unfocus("' + this.id + '", "' + this.focused_class + '")', duration);
-			} else {
-				var _this = this;
+			var _this = this;
 
-				this.$node.one('focusout', function() {
-					unfocus(_this.id, _this.focused_class);
-				});
+			var unfocus_callback = function() {
+				$("#" + _this.id).removeClass(_this.focused_class);
+			};
+
+			if (duration > 0) {
+				self.setTimeout(unfocus_callback, duration);
+			} else {
+				this.$node.one('focusout', unfocus_callback);
 			}
+		}
+	}
+
+	XHtml5FormElement.prototype._get_cleared_new_input = function($node) {
+		var new_input = '<input ';
+
+		var node_attribute_name;
+		var node_value;
+
+		for (var i = 0; i < NEW_INPUT_ATTRIBUTES_COPIED.length; i++) {
+			node_attribute_name = NEW_INPUT_ATTRIBUTES_COPIED[i];
+
+			node_value = $node.attr(node_attribute_name);
+			if (node_value !== undefined) { new_input += " " + node_attribute_name + "=\"" + node_value + "\""; }
+		}
+
+		new_input += '></input>';
+		var _return = $(new_input);
+
+		node_value = $node.data();
+		if (node_value !== undefined) { _return.data(node_value); }
+
+		return _return;
+	}
+
+	XHtml5FormElement.prototype._get_file_input_details = function(input_file_data) {
+		var _return = input_file_data.name;
+
+		if ('size' in input_file_data) {
+			_return += " <span class=\"" + this.selected_file_details_class + "\">(" + input_file_data.size + " B)</span>";
+		}
+
+		return _return;
+	}
+
+	XHtml5FormElement.prototype._init_file_input = function() {
+		var _this = this;
+
+		this.$node.on('change', function(event) {
+			_this.on_file_input_selected(event);
+		});
+	}
+
+	XHtml5FormElement.prototype.on_file_input_deselected = function(event, $selected_file_wrapper) {
+		if (this.$node.attr('multiple') === undefined) {
+			var $old_node = this.$node;
+			this.$node = this._get_cleared_new_input($old_node);
+			$old_node.remove();
+
+			$selected_file_wrapper.after(this.$node);
+			$selected_file_wrapper.remove();
+
+			this.$node.css('display', 'initial');
+			this._init_file_input();
+		} else {
+			$selected_file_wrapper.remove();
+		}
+	}
+
+	XHtml5FormElement.prototype.on_file_input_selected = function(event) {
+		if ('files' in event.target && event.target.files.length > 0) {
+			var selected_file_wrapper = "<div class=\"" + this.selected_file_class + "\"></div>";
+			var selected_files = event.target.files;
+
+			this.$node.wrap(selected_file_wrapper);
+			var $selected_file_wrapper = this.$node.parent();
+
+			this.$node.css('display', 'none');
+
+			if (selected_files.length == 1) {
+				$selected_file_wrapper.append("<span>" + this._get_file_input_details(selected_files[0]) + "</span>");
+			} else {
+				var $selected_files_list = $('<ul></ul>');
+
+				for (var i = 0; i < selected_files.length; i++) {
+					$selected_files_list.append('<li>' + this._get_file_input_details(selected_files[i]) + '</li>');
+				}
+
+				$selected_file_wrapper.append($selected_files_list);
+			}
+
+			if (this.$node.attr('multiple') !== undefined) {
+				var $old_node = this.$node;
+				this.$node = this._get_cleared_new_input($old_node);
+				$old_node.removeAttr('id');
+				$old_node.removeAttr('tabindex');
+
+				$selected_file_wrapper.after(this.$node);
+
+				this.$node.css('display', 'initial');
+				this._init_file_input();
+			}
+
+			var _this = this;
+
+			Hammer($selected_file_wrapper.get(0)).on('tap', function(event) {
+				_this.on_file_input_deselected(event, $selected_file_wrapper);
+			});
+		}
+	}
+
+	XHtml5FormElement.prototype.propagate_tabindex = function() {
+		if (this.$node.attr('tabindex') === undefined) {
+			this.$node.attr('tabindex', tabindex_count);
+			tabindex_count += 1;
 		}
 	}
 
@@ -133,169 +234,7 @@ define([ 'jquery', 'Modernizr' ], function($, Modernizr) {
 		this.temporarily_disabled_duration = duration;
 	}
 
-	XHtml5FormElement.prototype.tabindex = function() {
-		this.$node.attr('tabindex', tabindex_count);
-		tabindex_count += 1;
-	}
-
 	return XHtml5FormElement;
-/*
-			if (f_type == 'datepicker')
-			{
-				djs_load_functions([ { file:'ext_djs/djs_html5_datetime.min.js' } ]).done (function ()
-				{
-					if (!('css_classes' in f_params)) { f_params['css_classes'] = null; }
-					if (!('css_classes_replaced' in f_params)) { f_params['css_classes_replaced'] = null; }
-					djs_html5_datetime ({ datetime_lang:"<?php echo $direct_local['lang_iso_domain']; ?>",id:f_params.id,onCompleted:{ func:'djs_formbuilder_set_select_css',params:f_params } });
-				});
-			}
-
-			if (f_type == 'form')
-			{
-				if (djs_var.basic_formbuilder_form_ready)
-				{
-					if (!('id_button' in f_params)) { f_params['id_button'] = null; }
-					if (!('url' in f_params)) { f_params['url'] = null; }
-					jQuery("#" + f_params.id).on ('submit',function () { return djs_formbuilder_submit (f_params.id,f_params.url,f_params.id_button); });
-				}
-				else
-				{
-		djs_load_functions ([
-		 { file:'swg_AJAX.php.js',block:'djs_swgAJAX_insert' },
-		 { file:'ext_jquery/jquery.ui.core.min.js' },
-		 { file:'ext_jquery/jquery.ui.effect.min.js' },
-		 { file:'ext_jquery/jquery.ui.effect-transfer.min.js' }
-		]).done (function ()
-		{
-			djs_var.basic_formbuilder_form_ready = true;
-			djs_formbuilder_init (f_params);
-		});
-				}
-			}
-
-			if (f_type == 'form_sections')
-			{
-				if (djs_var.basic_formbuilder_accordion_ready)
-				{
-					jQuery("#" + f_params.id + " > .ui-accordion-header").removeClass ('pagecontenttitle');
-					jQuery("#" + f_params.id).accordion({ header:'.ui-accordion-header',heightStyle:'content' }).on ('activate',function (f_event,f_ui) { f_ui.newPanel.find(':input').first().trigger ('focus'); });
-				}
-				else
-				{
-		djs_load_functions ([
-		 { file:'ext_jquery/jquery.ui.core.min.js' },
-		 { file:'ext_jquery/jquery.ui.widget.min.js' },
-		 { file:'ext_jquery/jquery.ui.accordion.min.js' }
-		]).done (function ()
-		{
-			djs_var.basic_formbuilder_accordion_ready = true;
-			djs_formbuilder_init (f_params);
-		});
-				}
-			}
-
-			if (f_type == 'email')
-			{
-				var f_data = jQuery ("#" + f_params.id);
-				if (!('formNoValidate' in f_data.get (0))) { djs_DOM_replace ({ animate:false,data:(f_data.clone().attr ('type','text')),id:f_params.id,onReplace:{ func:'djs_formbuilder_init',params:{ id:f_params.id } } }); }
-			}
-
-			if (f_type == 'range')
-			{
-				var f_data = jQuery ("#" + f_params.id);
-
-				if (!('stepUp' in f_data.get (0)))
-				{
-					if (!djs_var.basic_formbuilder_range_ready)
-					{
-						djs_load_functions ({ file:'swg_formbuilder.php.js',block:'djs_formbuilder_range' });
-						djs_load_functions ({ file:'ext_jquery/jquery.ui.core.min.js' });
-						djs_load_functions ({ file:'ext_jquery/jquery.ui.widget.min.js' });
-						djs_load_functions ({ file:'ext_jquery/jquery.ui.mouse.min.js' });
-						djs_load_functions ({ file:'ext_jquery/jquery.ui.slider.min.js' });
-
-						djs_var.basic_formbuilder_range_ready = true;
-					}
-
-					f_data.next('br').remove ();
-
-					var f_range_params = { id:f_params.id,max:(parseFloat (f_data.attr ('max'))),min:(parseFloat (f_data.attr ('min'))),value:f_data.val () };
-					if (f_range_params.value == '') { f_range_params.value = f_range_params.min; }
-					f_data = "<div><div id='" + f_params.id + "s'></div><input type='hidden' name='" + (f_data.attr ("name")) + "' value=\"" + f_range_params.value + "\" id='" + f_params.id + "i' /><b><span id='" + f_params.id + "o'>" + f_range_params.value + "</span></b></div>";
-
-					djs_DOM_replace ({ data:f_data,id:f_params.id,onReplace:{ func:'djs_formbuilder_range',params:f_range_params } });
-				}
-			}
-
-			if (f_type == 'resizeable')
-			{
-				if (djs_var.basic_formbuilder_resizeable_ready) { jQuery("#" + f_params.id).wrap("<div style='padding:<?php echo $direct_settings['theme_form_td_padding']; ?>' />").parent().resizable ({ alsoResize:"#" + f_params.id }); }
-				else
-				{
-		djs_load_functions ([
-		 { file:'ext_jquery/jquery.ui.core.min.js' },
-		 { file:'ext_jquery/jquery.ui.widget.min.js' },
-		 { file:'ext_jquery/jquery.ui.mouse.min.js' },
-		 { file:'ext_jquery/jquery.ui.resizable.min.js' }
-		]).done (function ()
-		{
-			djs_var.basic_formbuilder_resizeable_ready = true;
-			djs_formbuilder_init (f_params);
-		});
-				}
-			}
-
-			if (f_type == 'search')
-			{
-				var f_data = jQuery ("#" + f_params.id);
-				if (!('formNoValidate' in f_data.get (0))) { djs_DOM_replace ({ animate:false,data:(f_data.clone().attr ('type','text')),id:f_params.id,onReplace:{ func:'djs_formbuilder_init',params:{ id:f_params.id } } }); }
-			}
-
-			if (f_type == 'tel')
-			{
-				var f_data = jQuery ("#" + f_params.id);
-				if (!('formNoValidate' in f_data.get (0))) { djs_DOM_replace ({ animate:false,data:(f_data.clone().attr ('type','text')),id:f_params.id,onReplace:{ func:'djs_formbuilder_init',params:{ id:f_params.id } } }); }
-			}
-
-			if (f_type == 'timepicker')
-			{
-				djs_load_functions([ { file:'ext_djs/djs_html5_datetime.min.js' } ]).done (function ()
-				{
-					if (!('css_classes' in f_params)) { f_params['css_classes'] = null; }
-					if (!('css_classes_replaced' in f_params)) { f_params['css_classes_replaced'] = null; }
-					djs_html5_datetime ({ datetime_lang:"<?php echo $direct_local['lang_iso_domain']; ?>",id:f_params.id,onCompleted:{ func:'djs_formbuilder_set_select_css',params:f_params } });
-				});
-			}
-
-			if (f_type == 'url')
-			{
-				var f_data = jQuery ("#" + f_params.id);
-				if (!('formNoValidate' in f_data.get (0))) { djs_DOM_replace ({ animate:false,data:(f_data.clone().attr ('type','text')),id:f_params.id,onReplace:{ func:'djs_formbuilder_init',params:{ id:f_params.id } } }); }
-			}
-		}
-
-		function djs_formbuilder_range (f_params) { f_jquery_object = jQuery("#" + f_params.id + "s").slider(f_params).on ('slide',function (f_event,f_ui) { djs_formbuilder_range_slide (f_params,f_ui.value); }); }
-
-		function djs_formbuilder_range_slide (f_params,f_value)
-		{
-			jQuery("#" + f_params.id + "i").val (f_value);
-			jQuery("#" + f_params.id + "o").text (f_value);
-		}
-
-		function djs_formbuilder_set_select_css (f_params)
-		{
-			if (('css_classes' in f_params)&&('css_classes_replaced' in f_params)&&('id' in f_params)&&(f_params.css_classes !== null)&&(f_params.css_classes_replaced !== null))
-			{
-				var f_jquery_object = jQuery ("#" + f_params.id);
-
-				if (f_jquery_object.data ('djsHtml5Replaced') == true) { f_jquery_object.find("select").filter(':visible').addClass (f_params.css_classes_replaced); }
-				else
-				{
-					f_jquery_object.addClass (f_params.css_classes);
-					djs_formbuilder_tabindex (f_params);
-				}
-			}
-*/
 });
 
 //j// EOF
